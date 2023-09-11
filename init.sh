3a49d17b79a03671eb2e0aa1b72c9856f571bde2
MikkoVasankari <vasankarimikko@gmail.com>
2023-09-11 09:17:38 +0300

    Added checks for required installations

--- init.sh
+++ init.sh
@@ -1,3 +1,42 @@
 #!/bin/bash
-	node bakkari/ & 
+
+nodeInstalled="false"
+pnpmInstalled="false"
+npmInstalled="false"
+
+if ! command -v npm > /dev/null;
+then
+	echo " npm installation not found "
+	exit
+else 
+	npmInstalled="true"
+fi
+
+
+if ! command -v node > /dev/null;
+then
+	echo " node installation not found "
+	exit
+else 
+	nodeInstalled="true"
+fi
+
+
+if ! command -v pnpm > /dev/null;
+then
+	echo " pnpm installation not found "
+	exit
+else 
+	pnpmInstalled="true"
+fi
+
+
+if nodeInstalled=="true" && pnpmInstalled=="true" && npmInstalled=="true"
+then
+	cd bakkari/ && npm install 
+
+	cd ..
+
+    node bakkari/ &  
 	cd ts_info/ && pnpm tauri dev
+fi
